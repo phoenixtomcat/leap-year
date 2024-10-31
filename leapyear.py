176b7a5935085c2ce858428fbefb91a81f4ab061
Ramtin <44302342+phoenixtomcat@users.noreply.github.com>
2024-10-31 15:34:18 -0400

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,17 @@
+print("What year do you have in mind?")
+year = int(input())
+
+# Calculate the leap year status
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not leap year")
+
+
+
